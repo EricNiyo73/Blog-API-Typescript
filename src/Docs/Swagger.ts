d3fa41b671275173212f0afa4d8886222163ce34
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2024-03-07 08:57:18 +0200

    handling servers in docs

--- src/Docs/Swagger.ts
+++ src/Docs/Swagger.ts
@@ -34,7 +34,7 @@ const options = {
     ],
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://my-brand-backend-ts.onrender.com",
         description: "Development server",
       },
     ],
