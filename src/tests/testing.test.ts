e8327d930dc921afaa04dc7214f76d8c3f54854a
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2024-03-13 14:53:28 +0200

    adding environments secretsk

--- src/tests/testing.test.ts
+++ src/tests/testing.test.ts
@@ -263,7 +263,7 @@ describe("POST /api/users/signup", () => {
   it("should POST a new user", async () => {
     const res = await request.post("/api/users/signup").send({
       fullName: "test",
-      email: "tokyo@test.com",
+      email: "tokyog@test.com",
       password: "password",
     });
 
@@ -423,7 +423,7 @@ describe("Blogs api testing", () => {
 
   it("should  add a blog and return success ", async () => {
     const newBlog = {
-      title: "testTokyo blog",
+      title: "testTokyoh blog",
       description: "test desc blog",
       image: "testImage.png",
     };
